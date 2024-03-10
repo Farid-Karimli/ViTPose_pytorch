569a3df05605b7602ff7615a3082d8f5610efb3c
Farid Karimli <karimli.farid@icloud.com>
2024-03-09 21:26:39 -0500

    Inference model path change

--- inference.py
+++ inference.py
@@ -83,7 +83,7 @@ if __name__ == "__main__":
     
     CUR_DIR = osp.dirname(__file__)
     # CKPT_PATH = f"{CUR_DIR}/vitpose-b-multi-coco.pth"
-    CKPT_PATH = "/home/jaehyun/workspace/PoseEstimation/ViTPose_pytorch/runs/train/002/epoch010.pth"
+    CKPT_PATH = f"{CUR_DIR}/weights/vitpose-b-multi-coco.pth"
     
     img_size = data_cfg['image_size']
     if type(args.image_path) != list:
